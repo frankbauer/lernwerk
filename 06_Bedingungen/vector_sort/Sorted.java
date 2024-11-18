bb7ded6eb5028601788ad1ecec23dbb2c7d5f889
susannaStroehmberg <susanna.stroehmberg@fau.de>
2024-11-18 23:40:48 +0100

    revision and more detailed explanation of task "2D Vektoren Sortieren"

--- 06_Bedingungen/vector_sort/Sorted.java
+++ 06_Bedingungen/vector_sort/Sorted.java
@@ -4,19 +4,21 @@ public class Sorted {
 //#START STUDENT
         
 //#START SOLUTION
-        // Erstellen des ersten Vektors mit zufälligen x- und y-Werten im Bereich [-25, 25[
+        // Erstellen des ersten Vektors mit zufaelligen x- und y-Werten im Bereich [-25, 25[:
         Vec2D v1 = new Vec2D(Math.random()*50 - 25, Math.random()*50 - 25);
-        // Berechnen des Winkels zwischen dem normalisierten v1 und der X-Achse
-        // v1 normalisieren
+
+        // Berechnen des Winkels zwischen dem normalisierten v1 und der X-Achse:
+        //1.) v1 normalisieren
         Vec2D v1Normalized = v1.normalize();
-        // Skalarprodukt zwischen v1 und der X-Achse berechnen
+        //2.) Skalarprodukt zwischen v1 und der X-Achse berechnen
         double scalarProduct = Vec2D.XAxis.dot(v1Normalized);
-        // Winkel (in Radianten) bestimmen
+        //3.) Winkel (in Radianten) bestimmen
         double w1 = Math.acos(scalarProduct);
 
-        // Erstellen des zweiten Vektors mit zufälligen x- und y-Werten im Bereich [-25, 25[
-        Vec2D v2 = new Vec2D(Math.random()*50 - 25, Math.random()*50 - 25);        
-        // Berechnen des Winkels zwischen dem normalisierten v2 und der X-Achse
+        // Erstellen des zweiten Vektors mit zufaelligen x- und y-Werten im Bereich [-25, 25[:
+        Vec2D v2 = new Vec2D(Math.random()*50 - 25, Math.random()*50 - 25);  
+
+        // Berechnen des Winkels zwischen dem normalisierten v2 und der X-Achse:
         double w2 = Math.acos(Vec2D.XAxis.dot(v2.normalize()));
         
         // Vergleich der Winkel und Ausgabe der Vektoren in aufsteigender Reihenfolge nach dem Winkel
