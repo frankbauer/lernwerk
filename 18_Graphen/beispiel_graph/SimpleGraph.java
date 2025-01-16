173c5db328b60c394c643bb8788a9aded335985f
susannaStroehmberg <susanna.stroehmberg@fau.de>
2025-01-16 12:28:49 +0100

    addition of explanation and revision of task "Spezialdatenstruktur für Graphen"

--- 18_Graphen/beispiel_graph/SimpleGraph.java
+++ 18_Graphen/beispiel_graph/SimpleGraph.java
@@ -1,13 +1,21 @@
 public class SimpleGraph {
     public static void main(String[] args){
+        //Erstellen eines neuen Graphen mit Elementen
+        //vom Datentyp "String"
         Graph<String> g = new Graph<>();
 
+        //Hinzufuegen von 5 Knoten zum Graphen g
         g.add("Berlin"); g.add("Amsterdam"); 
         g.add("London"); g.add("Paris"); g.add("Rom");
 
+        //Fuegt eine gerichtete Kante von "Berlin" 
+        //nach "Amsterdam" mit Gewicht 1 hinzu
         g.addEdge("Berlin", "Amsterdam");
+        //Fuegt eine gerichtete Kante von "Amsterdam"
+        //nach "Berlin" mit Gewicht 1 hinzu
         g.addEdge("Amsterdam", "Berlin");
 
+        //Hinzufuegen weiterer Kanten
         g.addEdge("Paris", "Amsterdam");
         g.addEdge("Amsterdam", "Paris");
 
@@ -19,7 +27,10 @@ public class SimpleGraph {
 
         g.addEdge("Berlin", "Rom");
         g.addEdge("Rom", "Berlin");
-             
+
+        //Visualisierung des Graphen:
+        //die Position der Knoten kann per Drag & Drop
+        //geaendert werden     
         g.visualize();
     }
 }
