b4b4996d6bb8ac5ec920491d19ff29661430a4f5
Jakob_Schade <jakob.schade@fau.de>
2025-01-20 13:10:33 +0100

    MeineSumme Kommentare, wo was geschrieben wird

--- 08_Klassenmethoden/sandkasten_summe/MeineSumme.java
+++ 08_Klassenmethoden/sandkasten_summe/MeineSumme.java
@@ -1,11 +1,11 @@
 //#START STATIC
 public class MeineSumme {
 //#START STUDENT        
-    
+    //berechneSumme
 //#START STATIC
     public static void main(String[] args){
 //#START STUDENT        
-        
+    //Aufrufen von berechneSumme
 //#START STATIC
     }
 }
